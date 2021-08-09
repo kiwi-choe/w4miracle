a07b6ea7d73e2fbcd237a31819264bc819e264d5
kiwi-choe <magmag0221@gmail.com>
2021-08-10 00:34:02 +0900

    gallary view만들기

--- script/upload.js
+++ script/upload.js
@@ -1,170 +1,151 @@
-const body = document.querySelector('body');
-const elGalleryList = document.querySelector('.galleryList');
-const form = document.querySelector('#uploadForm');
-const elFile = document.querySelector('.uploadForm__file');
-const elMessage = document.querySelector('.uploadForm__text');
-const elPrevImgWrap = document.querySelector('.prevImgWrap');
+const body = document.querySelector("body");
+const elGalleryList = document.querySelector(".galleryList");
+const form = document.querySelector("#uploadForm");
+const elFile = document.querySelector(".uploadForm__file");
+const elMessage = document.querySelector(".uploadForm__text");
+// const elPrevImgWrap = document.querySelector('.prevImgWrap');
 
-// Initialize Firebase
+// [START] Initialize Firebase
 const firebaseConfig = {
-	apiKey: "AIzaSyCg-XnCKH6zScJY_04rXUf0Fmxbza_JnGU",
-	authDomain: "walking4miracle.firebaseapp.com",
-	projectId: "walking4miracle",
-	storageBucket: "walking4miracle.appspot.com",
-	messagingSenderId: "414251762442",
-	appId: "1:414251762442:web:242b6a090d8013a7d9f0f3",
-	measurementId: "G-X0KSZNYM6V",
+  apiKey: "AIzaSyCg-XnCKH6zScJY_04rXUf0Fmxbza_JnGU",
+  authDomain: "walking4miracle.firebaseapp.com",
+  projectId: "walking4miracle",
+  storageBucket: "walking4miracle.appspot.com",
+  messagingSenderId: "414251762442",
+  appId: "1:414251762442:web:242b6a090d8013a7d9f0f3",
+  measurementId: "G-X0KSZNYM6V",
 };
 
 firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage();
 const db = firebase.firestore();
 db.settings({
-	timestampsInSnapshots: true,
+  timestampsInSnapshots: true,
 });
 
-const COL_PHOTOS = 'photos';
+const COL_PHOTOS = "photos";
+// Initialize Firebase [END]
 
 function init() {
-	selectPhotos();
+  console.log("init");
+  selectPhotos();
 }
 init();
 
 // preview
-elFile.addEventListener('change', (event) => {
-	event.preventDefault();
-	const {
-		target: {
-			files
-		}
-	} = event;
-	const theFile = files[0];
-	const reader = new FileReader();
-	elPrevImgWrap.textContent = '';
+// elFile.addEventListener("change", (event) => {
+//   event.preventDefault();
+//   const {
+//     target: { files },
+//   } = event;
+//   const theFile = files[0];
+//   const reader = new FileReader();
+//   elPrevImgWrap.textContent = "";
 
-	reader.onloadend = (finishedEvent) => {
-		const {
-			currentTarget: {
-				result
-			}
-		} = finishedEvent;
-		let previewImg = document.createElement('img');
+//   reader.onloadend = (finishedEvent) => {
+//     const {
+//       currentTarget: { result },
+//     } = finishedEvent;
+//     let previewImg = document.createElement("img");
 
-		previewImg.style.width = '100px';
-		previewImg.style.height = '100px';
-		previewImg.setAttribute('src', result);
-		elPrevImgWrap.appendChild(previewImg)
-	}
-	reader.readAsDataURL(theFile);
-})
+//     previewImg.style.width = "100px";
+//     previewImg.style.height = "100px";
+//     previewImg.setAttribute("src", result);
+//     elPrevImgWrap.appendChild(previewImg);
+//   };
+//   reader.readAsDataURL(theFile);
+// });
 
 // submit
-form.addEventListener('submit', onSubmit)
+form.addEventListener("submit", onSubmit);
 
 async function onSubmit(event) {
-	event.preventDefault();
-	const theFile = document.querySelector('.uploadForm__file').files[0];
+  event.preventDefault();
+  const photo = document.querySelector(".uploadForm__file").files[0];
 
-	try {
-		if (!validCheck(theFile, elMessage.value)) return;
+  try {
+    if (!validatePhoto(photo)) return;
 
-		const fileURL = await getFileURL(theFile);
-		const posting = getPosting(fileURL, elMessage.value);
+    const photoUrl = await getFileURL(photo);
+    const posting = getPosting(photoUrl);
 
-		addPhotos(posting);
-		initView();
-		prependGallery(posting);
-
-	} catch (e) {
-		throw e;
-	}
+    addPhoto(posting);
+    // initView();
+    prependGallery(posting);
+  } catch (e) {
+    throw e;
+  }
 }
 
 async function selectPhotos() {
-	const photos = await getPhotos();
-	viewPhotos(photos)
+  const photos = await getPhotos();
+  viewPhotos(photos);
 }
 
 async function getPhotos() {
-	const photos = await db
-		.collection(COL_PHOTOS)
-		.orderBy("createdAt", "desc")
-		.get()
-		.then(snapshot => {
-			return snapshot.docs.map((doc) => doc.data());
-		})
-	return photos;
+  const photos = await db
+    .collection(COL_PHOTOS)
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((snapshot) => {
+      return snapshot.docs.map((doc) => doc.data());
+    });
+  return photos;
 }
 
 function viewPhotos(photos) {
-	if (photos.length !== 0) {
-		photos.map(gallery => {
-			const elLi = document.createElement('li');
-			const elImg = document.createElement('img');
-			const elSpan = document.createElement('span');
-
-			elImg.setAttribute('src', gallery.fileURL);
-			elImg.style.width = '100px';
-			elImg.style.height = '100px';
-			elSpan.textContent = gallery.message;
-
-			elLi.append(elImg, elSpan);
-			elGalleryList.appendChild(elLi)
-		})
-	}
+  if (photos.length !== 0) {
+    photos.map((photo) => {
+      prependGallery(photo);
+    });
+  }
 }
 
-function validCheck(theFile, message) {
-	if (!theFile) {
-		alert('사진을 선택해주세요.')
-		return false;
-	}
-	if (!message) {
-		alert('응원메시지를 남겨주세요!')
-		return false;
-	}
-	return true
+function validatePhoto(theFile) {
+  if (!theFile) {
+    alert("사진을 선택해주세요.");
+    return false;
+  }
+  return true;
 }
 
 async function getFileURL(theFile) {
-	const today = new Date().toISOString();
-	const storageRef = storage.ref();
-	const path = storageRef.child(`image/${today}`);
-	const response = await path.put(theFile);
-	const fileURL = await response.ref.getDownloadURL();
-	return fileURL;
+  const today = new Date().toISOString();
+  const storageRef = storage.ref();
+  const path = storageRef.child(`image/${today}`);
+  const response = await path.put(theFile);
+  const fileURL = await response.ref.getDownloadURL();
+  return fileURL;
 }
 
-function getPosting(fileURL, message) {
-	const today = new Date().toISOString();
-	return {
-		createdAt: today,
-		fileURL: fileURL,
-		message: message
-	}
+function getPosting(fileURL) {
+  const today = new Date().toISOString();
+  return {
+    createdAt: today,
+    fileURL: fileURL,
+    // message: message,
+  };
 }
 
-async function addPhotos(post) {
-	await db
-		.collection(COL_PHOTOS)
-		.add(post)
+async function addPhoto(post) {
+  await db.collection(COL_PHOTOS).add(post);
 }
 
-function initView() {
-	elMessage.value = '';
-	elPrevImgWrap.textContent = '';
-}
+// function initView() {
+//   elMessage.value = "";
+//   elPrevImgWrap.textContent = "";
+// }
 
 function prependGallery(posting) {
-	const elLi = document.createElement('li');
-	const elImg = document.createElement('img');
-	const elSpan = document.createElement('span');
+  const elDiv = document.createElement("div");
+  const elImg = document.createElement("img");
 
-	elImg.setAttribute('src', posting.fileURL);
-	elImg.style.width = '100px';
-	elImg.style.height = '100px';
-	elSpan.textContent = posting.message;
+  elImg.setAttribute("src", posting.fileURL);
+  elDiv.setAttribute("class", "item");
+  //   elImg.style.width = "100px";
+  //   elImg.style.height = "100px";
+  //   elSpan.textContent = posting.message;
 
-	elLi.append(elImg, elSpan);
-	elGalleryList.prepend(elLi)
+  elDiv.append(elImg);
+  elGalleryList.prepend(elDiv);
 }
